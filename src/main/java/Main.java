7b8024f8e55c57b33f4b5dfdb4d261135654c03b
ogar_m <ogar.m.v@maio.ru>
2025-04-16 09:33:54 +0300

    garbage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         Plastic firstPartPlastic = new Plastic("Полиэтилен"); //Пакет
-        Plastic secondPartPlastic = new Plastic("Полистирол"); //Корпус
 
         Glass firstPartGlass = new Glass("Армированное"); //Усиленное окно
-        Glass secondPartGlass = new Glass("Сталинит"); //Защитное стекло на телефон
+
+        Paper firstPartPaper = new Paper("Газетная бумага"); //Газета
 
         Box<Plastic> boxForPlastic = new Box<>();
         boxForPlastic.putItem(firstPartPlastic);
@@ -14,5 +18,8 @@ public class Main {
         boxForGlass.putItem(firstPartGlass);
         System.out.println("Вторая партия мусора " + boxForGlass.getItem().display());
 
+        Box<Paper> boxForPaper = new Box<>();
+        boxForPaper.putItem(firstPartPaper);
+        System.out.println("Третья партия мусора " + boxForPaper.getItem().display());
     }
 }
---
+++ src/main/java/TwoCellsBox.java
@@ -0,0 +1,4 @@
+public class TwoCellsBox<T, S> {
+    private T firstCellBox;
+    private S secondCellBox;
+}
